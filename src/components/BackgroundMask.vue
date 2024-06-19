e2b635757f1f0ca53560827ba73d73e41e391bb8
wangshangxian <ws_xian@163.com>
2024-06-19 13:06:04 +0800

    backgrdounmask 的背景改成可配置

--- src/components/BackgroundMask.vue
+++ src/components/BackgroundMask.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="w-screen h-screen fixed top-0 left-0" style="z-index: 999;">
+  <div :class="box_class" style="z-index: 999;">
     <div class="w-full h-full absolute z-0" @click="onbgclick"></div>
     <slot></slot>
   </div>
 </template>
 
 <script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  bg: {
+    type: String,
+    default: 'bg-transparent'
+  }
+})
 const emits = defineEmits(['bgclick'])
 const onbgclick = () => emits('bgclick')
+
+const box_class = computed(() => {
+  const arrs = ['w-screen', 'h-screen', 'fixed', 'top-0', 'left-0']
+  arrs.push(props.bg)
+  return arrs
+})
 </script>
