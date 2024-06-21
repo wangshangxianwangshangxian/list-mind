a57d5ab149df8426acaea5262e00b2b7f9f2c14b
wangshangxian <ws_xian@163.com>
2024-06-21 13:59:41 +0800

    新增客服和版本入口

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router
 import HomeView from '../views/HomeView.vue'
 import MindView from '../views/MindView.vue'
 import SettingView from '@/views/SettingView.vue'
+import CustomerView from '@/views/CustomerView.vue'
+import VersionView from '@/views/VersionView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -25,6 +27,16 @@ const router = createRouter({
       path: '/setting/:tab',
       name: 'setting',
       component: SettingView
+    },
+    {
+      path: '/customer',
+      name: 'customer',
+      component: CustomerView
+    },
+    {
+      path: '/version',
+      name: 'version',
+      component: VersionView
     }
   ]
 })
